8940ea7c888007be9c26eedd43f2f3c462002e3e
MarthaDs <marthadntas@gmail.com>
2018-11-30 18:20:15 -0200

    [CREATE] criando funções de criação da camada oculta

--- rede_neural.cpp
+++ rede_neural.cpp
@@ -4,46 +4,60 @@
 
 typedef struct neuronio {
     double entrada[537];
-    double peso;
+    double peso_b;
+    double pesos_w[537];
     double saida;
-    int camada;
-    int posicao;
-    struct camdaSeguinte *prox_neuronios;
+    struct neuronio *prox;
 }Neuronio;
 
-typedef struct camdaSeguinte {
-    void *neuronio;
-    struct camdaSeguinte *prox;
-}camdaSeguinte;
+typedef struct header {
+    int qntd_elemetos;
+    Neuronio * head;
+    Neuronio *tail;
+} Header;
 
+Header *inicializaHeader();
 double randPeso();
+Neuronio *criaNeuronio(double entrada[537]);
+void pesosW(double *pesos_w);
+void entradaNeuronio(Neuronio* neuronio, double entrada[537]);
+double funcaoAtivacao(int n);
+void addNeuronioCamadaOculta(Header *header, double entrada[537]);
+double camadaOculta(Header *header, int qntd_neuronio_oculta, double entrada[537]);
 
 int main (int argc, char *argv[]) {
 	char *qntd_terminal = argv[1];
 	int qntd_neuronio_oculta = atoi(qntd_terminal);
-	printf("Qnd de neuronio na camada oculta: %d\n",qntd_neuronio_oculta);
+    Header *header = inicializaHeader();
+    double entrada[537] = {0.0};
 
+    printf("Qnd de neuronio na camada oculta: %d\n",qntd_neuronio_oculta);
+    camadaOculta(header,qntd_neuronio_oculta, entrada);
 
 
     return 0;
 }
 
-Neuronio CriaNeuronio(double entrada[537], int camada) {
+Header *inicializaHeader() {
+	Header *header = (Header*) malloc(sizeof(Header));
+    header->qntd_elemetos = 0;
+    header->head = NULL;
+    header->tail = NULL;
+	return header;
+}
+
+Neuronio *criaNeuronio(double entrada[537]) {
     Neuronio *novoNeuronio = (Neuronio*) malloc(sizeof(Neuronio));
 
-    (novoNeuronio)->entrada = entrada;
-    novoNeuronio->camada = camada;
-    novoNeuronio->prox_neuronios = NULL;
+    entradaNeuronio(novoNeuronio, entrada);
+    novoNeuronio->peso_b = randPeso();
+    pesosW(novoNeuronio->pesos_w);
+    novoNeuronio->saida = 0;
 
-    int b = 1;
-    double n = 0;
-    
-    for(int i = 0;i < 537; i++) {
-        n += entrada[i] * randPeso();    
+    if(novoNeuronio != NULL) {
+        novoNeuronio->prox = NULL;
     }
-    n += b;
-    novoNeuronio->saida = funcaoAtivacao(n);
-
+    return novoNeuronio;
 }
 
 double funcaoAtivacao(int n) {
@@ -65,3 +79,33 @@ double randPeso() {
     return p;
 }
 
+void pesosW(double *pesos_w) {
+    for(int i = 0; i < 536; i++) {
+        pesos_w[i] = randPeso();
+    }
+}
+
+void entradaNeuronio(Neuronio* neuronio, double entrada[537]) {
+    for(int i = 0; i < 536; i++) {
+        neuronio->entrada[i] = entrada[i];
+    }
+}
+
+void addNeuronioCamadaOculta(Header *header, double entrada[537]) {
+        if(header->head == NULL) {
+            header->head = criaNeuronio(entrada);
+            header->tail = header->head;
+            header->qntd_elemetos++;
+        }
+        else {
+            header->tail->prox = criaNeuronio(entrada);
+            header->tail = header->tail->prox;
+            header->qntd_elemetos++;
+        }
+}
+
+double camadaOculta(Header *header, int qntd_neuronio_oculta, double entrada[537]) {
+    for(int i = 0;i <= qntd_neuronio_oculta; i++) {
+        addNeuronioCamadaOculta(header, entrada);
+    }
+}
